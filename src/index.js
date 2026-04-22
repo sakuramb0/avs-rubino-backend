ce64dd9d4bd426079b124659e87ec1389b511e88
Stefano Orlando <eorlste@ericsson.com>
2026-04-22 14:19:55 +0200

    feat: add public content route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const contentRoutes = require('./routes/content');
 const uploadRoutes = require('./routes/upload');
+const publicRoutes = require('./routes/public');
 const authMiddleware = require('./middleware/auth');
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/admin/content', authMiddleware, contentRoutes);
 app.use('/api/admin/upload', authMiddleware, uploadRoutes);
+app.use('/api/public/content', publicRoutes);
 
 const port = process.env.PORT || 8080;
 
---
+++ src/routes/public.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const admin = require("../config/firebase");
+
+const db = admin.firestore();
+const collection = db.collection('clinic_content');
+
+// GET all content (Public)
+router.get('/', async (req, res) => {
+  try {
+    const snapshot = await collection.get();
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
