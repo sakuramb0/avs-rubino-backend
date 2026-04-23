9d5f6cdeaf55e697895c6f2e1229f14b07a599bb
Stefano Orlando <eorlste@ericsson.com>
2026-04-23 16:14:27 +0200

    update log for firebase parameter

--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ console.log("Project ID:", process.env.FIREBASE_PROJECT_ID);
 console.log("Storage Bucket:", process.env.FIREBASE_STORAGE_BUCKET);
 console.log("==================================");
 
+db.collection('clinic_content').limit(1).get()
+  .then(snapshot => {
+    console.log("✅ FIRESTORE VIVO E VEGETO! Documenti trovati:", snapshot.size);
+  })
+  .catch(error => {
+    console.error("❌ ERRORE DIRETTO FIRESTORE:", error);
+  });
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
